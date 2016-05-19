363fd2f8f91839766e7dcd5a4e326bced7b1b8a6
Nicolas Ducom <nicolas.ducom@ext.adeoservices.com>
2016-05-19 22:01:47 +0200

    testCommit

--- routes/properties.js
+++ routes/properties.js
@@ -90,7 +90,7 @@ router.put('/', function (req,res) {
             property.addressLine1 = data.addressLine1;
             property.addressLine2 = data.addressLine2;
             property.city = data.city;
-            property.zip = data.zipCode;
+            property.zipCode = data.zipCode;
 
             property.save(function (err) {
                 if (err) {
